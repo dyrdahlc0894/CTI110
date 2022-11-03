6e1b7d7c9a02f35d003a7c109aab140f03a45121
dyrdahlc0894 <113455752+dyrdahlc0894@users.noreply.github.com>
2022-11-03 10:53:03 -0400

    Add files via upload

---
+++ P4LAB1aDyrdahl.py
@@ -0,0 +1,10 @@
+import turtle
+for x in range (4):
+  turtle.forward(50)
+  turtle.left(90)
+turtle.penup()
+turtle.right(180)
+turtle.pendown()
+for x in range (3):
+  turtle.forward(50)
+  turtle.left(120)
---
+++ P4LAB1bDyrdahl.py
@@ -0,0 +1,14 @@
+import turtle
+for x in range (19):
+  turtle.right(10)
+  turtle.back(10)
+turtle.penup()
+turtle.back(30)
+turtle.pendown()
+#turtle.left(90)
+#turtle.forward(120)
+for x in range (19):
+  turtle.back(10)
+  turtle.right(10)
+turtle.left(100)
+turtle.forward(120)
